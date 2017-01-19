82c23807a0e9378be0837ff711e4e4d5ce22eb3f
SWEsyndicate <elias_kingen@hotmail.com>
2017-01-19 14:55:13 +0100

    Klar

---
+++ Hello World!.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World_
+{
+    class Hello_World_
+    {
+        public void HelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+                
+
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hello_World_ hello = new Hello_World_();
+            hello.HelloWorld();
+        }
+    }
+}
